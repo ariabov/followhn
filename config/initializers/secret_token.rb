bf923f3993670ea52088224977903c46e4017d39
ariabov <spralex@gmail.com>
2013-09-30 19:58:00 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Followhn::Application.config.secret_token = '7b347b98384b8b8f3f8b8b005467ca480fcadf3661569cf4c250ef7d3e9e9f43dcfadda4188f7aa9602044298dccdab1682401436380631a0711953fdc43e103'
